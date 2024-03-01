e768173a6db6624cb652d197c443777d9c81d437
Luciano Cordeiro <lacs@petrobras.com.br>
2024-03-01 17:10:25 -0300

    Initial commit

---
+++ conceitos/frontend/public/script.js
@@ -0,0 +1,15 @@
+/**
+ * Babel: Converter (transpilar) código do React para um código que o browser entenda
+ * 
+ * Webpack: Para cada tipo de arquivo (.js, .css, .png, etc ) eu vou converter o código de uma maneira diferente
+ * 
+ *      - Loaders: babel-loader, css-loader, image-loader
+ * 
+ * 
+ *  -- Na criação da pasta: yarn init -y 
+ * 
+ * Instalar: 
+ * 
+ *  1 - yarn add react react-dom
+ *  2 - yarn add @babel/core @babel/preset-env @babel/preset-react webpack webpack-cli
+ */
