c06c8ece6893286731ca36b43fbe9e6a0bb78d2a
Luciano Cordeiro <lacs@petrobras.com.br>
2024-04-11 14:51:46 -0300

    State and immutability - end

--- conceitos/frontend/src/App.js
+++ conceitos/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
@@ -13,10 +13,20 @@ import Header from './components/Header';
 
 function App() {
 
-    const projects = ['Desenvolvimento de Apps', 'Frontend Web'];
+    // useState return an Array with 2 positions
+    //
+    // 1. Position - Variable with your initial value
+    // 2. Position - Function for updating this value
+
+    const [projects, setProjects] = useState(['Desenvolvimento de Apps', 'Frontend Web']);
 
     function handleAddProject(){
-        projects.push(`Novo projeto ${Date.now()}`);
+
+        //projects.push(`Novo projeto ${Date.now()}`);
+        // ...projects -> Spread Operator - Copy the Array, go through the entire array and add the new elements
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+        console.log(projects);
     }
 
     return (
