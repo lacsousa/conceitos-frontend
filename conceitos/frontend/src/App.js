95843098bcfdefde8522da3ad9e725aff078603c
Luciano Cordeiro <lacs@petrobras.com.br>
2024-04-12 16:17:01 -0300

    Listing API Projects

--- conceitos/frontend/src/App.js
+++ conceitos/frontend/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import officeImage from './assets/office.jpg';
-
 import Header from './components/Header';
+
+import api from './services/api';
 
 /**
  * Tudo que vc precisa saber no React
@@ -14,6 +14,9 @@ import Header from './components/Header';
  * 
  */
 
+// Install Axios - It will call our API
+// yarn add axios
+
 function App() {
 
     // useState return an Array with 2 positions
@@ -21,7 +24,18 @@ function App() {
     // 1. Position - Variable with your initial value
     // 2. Position - Function for updating this value
 
-    const [projects, setProjects] = useState(['Desenvolvimento de Apps', 'Frontend Web']);
+    //const [projects, setProjects] = useState(['Desenvolvimento de Apps', 'Frontend Web']);
+
+    // Always start the useState with the same type of the const. 
+    // How projects is an array, we use [] no useState
+    const [projects, setProjects] = useState([]); 
+
+    useEffect(() => {
+        api.get('projects').then( response =>{
+            setProjects(response.data);
+            console.log(response);
+        })
+    }, []);
 
     function handleAddProject(){
 
@@ -38,7 +52,7 @@ function App() {
 
                 <img width={300} src={officeImage} />
                 <ul>
-                    {projects.map( project => <li key={project}>{project}</li>)}
+                    {projects.map( project => <li key={project.id}>{project.title}</li>)}
                 </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
